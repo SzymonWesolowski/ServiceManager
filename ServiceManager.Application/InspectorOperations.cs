a391560b3c4f61bac4ece4955c7c9b4c24cb8b6a
Szymon Wesolowski <sw@maski.com.pl>
2020-01-19 19:36:31 +0100

    serviceman

---
+++ ServiceManager.Application/InspectorOperations.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ServiceManager.Domain.Model;
+
+namespace ServiceManager.Application
+{
+    internal interface IInspectorOperations
+    {
+        void AddInspector(string firstName, string lastName, string city, string phoneNumber);
+        List<Inspector> GeInspectorList();
+        void ModifyInspector(Inspector oldInspector, Inspector newInspector);
+        void RemoveInspector(Inspector inspector);
+    }
+
+    class InspectorOperations : IInspectorOperations
+
+    {
+        private readonly IInspectorRepository _inspectorRepository;
+
+        public InspectorOperations(IInspectorRepository inspectorRepository)
+        {
+            _inspectorRepository = inspectorRepository;
+        }
+        public void AddInspector(string firstName, string lastName, string city, string phoneNumber)
+        {
+            var inspector = new Inspector(firstName, lastName, city, phoneNumber);
+            _inspectorRepository.AddInspector(inspector);
+        }
+
+        public List<Inspector> GeInspectorList()
+        {
+            return _inspectorRepository.GetInspectorList();
+        }
+
+        public void ModifyInspector(Inspector oldInspector, Inspector newInspector)
+        {
+            _inspectorRepository.ModifyInspector(oldInspector, newInspector);
+        }
+
+        public void RemoveInspector(Inspector inspector)
+        {
+            _inspectorRepository.RemoveInspector(inspector);
+        }
+    }
+}
