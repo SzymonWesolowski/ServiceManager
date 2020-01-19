35d7093446f1dc608449a4c43792dede4081e2c7
Szymon Wesolowski <sw@maski.com.pl>
2020-01-19 15:33:10 +0100

    estateOperations/RemoveEnums

--- ServiceManager.Domain/Model/Inspector.cs
+++ ServiceManager.Domain/Model/Inspector.cs
@@ -2,7 +2,7 @@
 {
     public class Inspector
     {
-        public Inspector(string firstName, string lastName, CityEnum city, string phoneNumber)
+        public Inspector(string firstName, string lastName, string city, string phoneNumber)
         {
             FirstName = firstName;
             LastName = lastName;
@@ -12,7 +12,7 @@
 
         public string FirstName { get; }
         public string LastName { get; }
-        public CityEnum City { get; }
+        public string City { get; }
         public string PhoneNumber { get; }
     }
 }
